2a84cfc3a72d8989358d2a1ad0f6226d8f2772c0
Gabriel Luna <lunagabri@gmail.com>
2022-11-01 11:56:47 -0300

    Início do exercício

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import serverApi from "./api/servidor-api";
 
 function App() {
+  const [produtos, setProdutos] = useState([]);
+
+  useEffect(() => {
+    async function getProdutos() {
+      try {
+        const resposta = await fetch(`https://fakestoreapi.com/products`);
+        const dados = await resposta.json();
+        setProdutos(dados);
+        console.log(dados);
+      } catch (error) {
+        console.log("Deu ruim! " + error.message);
+      }
+    }
+    getProdutos();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {produtos.map(
+        ({ id, title, price, image }) => (
+          (id = { id }),
+          (image = { image }),
+          (title = { title }),
+          (price = { price })
+        )
+      )}
     </div>
   );
 }
